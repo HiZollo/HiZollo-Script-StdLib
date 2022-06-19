cc9821112729afe0578794c0e7e70ba9f5155b69
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2022-06-19 21:47:59 +0800

    chore: update build tool so it looks cooler

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import Uglify from 'uglify-js';
 import { readdirSync, readFileSync, writeFileSync, existsSync, mkdirSync } from 'node:fs';
 
+const n = Date.now();
+
 const files = readdirSync(`./src`).filter(file => file.endsWith('.js'));
 
 if (!existsSync('./build')) {
@@ -8,9 +10,21 @@ if (!existsSync('./build')) {
 }
 
 for (const file of files) {
+	console.log(`[${file}] 讀取檔案中⋯⋯`);
 	const code = readFileSync(`./src/${file}`, 'utf-8');
+	console.log(`[${file}] 檔案讀取完畢`);
+	console.log(`[${file}] 最小化中⋯⋯`);
 	const result = Uglify.minify(code);
 
-	if (!result.error) writeFileSync(`./build/${file}`, result.code);
-	else console.error(result.error);
+	if (!result.error) {
+		console.log(`[${file}] 最小化完畢`);
+		console.log(`[${file}] 輸出至 /build/${file}⋯⋯`);
+		writeFileSync(`./build/${file}`, result.code);
+		console.log(`[${file}] 已輸出`);
+	} else {
+		console.log(`[${file}] 出現錯誤，錯誤內容：`);
+		console.error(result.error);
+	}
 }
+
+console.log(`\nBuild Step Complete\nTime: ${Date.now() - n}ms`);
