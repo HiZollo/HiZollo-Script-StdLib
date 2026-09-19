383d4f29eb58ef801995e7afedc50b6a0944209a
AC0xRPFS001 <ac0xrpfs001@vivaldi.net>
2026-09-20 05:05:56 +0800

    feature: print format string

--- src/io.js
+++ src/io.js
@@ -1,6 +1,10 @@
 (function () {
+  function format(str, ...args) {
+    let index = 0;
+    return str.replace(/%s/g, () => (index < args.length ? args[index++] : '%s'));
+  }
   __hzs_export("std.io", {
-    print: function(str) { _write(`${str}`) },
-    println: function(str) { _write(`${str}\n`) },
+    print: function(fmt = '', ...args) { _write(format(fmt, ...args)) },
+    println: function(fmt = '', ...args) { _write(format(`${fmt}\n`, ...args)) },
   })
 })()
